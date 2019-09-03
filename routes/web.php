1903b1233847a0649dace036b9977519340a2552
Hadi Behmanesh <haadibehmanesh@gmail.com>
2019-09-03 18:00:53 +0430

    add coming soon package

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});*/
+Route::get('/', function () {
+  return view('comingsoon::comingsoon');
 });
-
 
 Route::group(['prefix' => 'admin'], function () {
  
