b14f0a252b26115de319c4bcff0f74c37f1dc1f8
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-29 13:53:20 +0200

    add searchable trait for news

--- app/News.php
+++ app/News.php
@@ -3,24 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Nicolaslopezj\Searchable\SearchableTrait;
+
 
 class News extends Model
 {
-    use SearchableTrait;
-    protected $searchable = [
-        /**
-         * Columns and their priority in search results.
-         * Columns with higher values are more important.
-         * Columns with equal values have equal importance.
-         *
-         * @var array
-         */
-        'columns' => [
-            'news.title' => 10,
-            'news.abstract' => 9,
-           
-        ],
-
-    ];
+    
 }
